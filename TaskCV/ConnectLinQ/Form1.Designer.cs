f2db4badaa6989169195853ad6819a5f7b8bbfcd
nguyenven299 <nguyenven1999@gmail.com>
2020-05-15 20:18:32 +0700

    connect linQ C# update

---
+++ TaskCV/ConnectLinQ/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace ConnectLinQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdUser = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmailUer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSelectIImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // txtIdUser
+            // 
+            this.txtIdUser.Location = new System.Drawing.Point(129, 6);
+            this.txtIdUser.Name = "txtIdUser";
+            this.txtIdUser.Size = new System.Drawing.Size(100, 20);
+            this.txtIdUser.TabIndex = 2;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(129, 33);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(301, 63);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(301, 30);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(301, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email";
+            // 
+            // txtEmailUer
+            // 
+            this.txtEmailUer.Location = new System.Drawing.Point(129, 66);
+            this.txtEmailUer.Name = "txtEmailUer";
+            this.txtEmailUer.Size = new System.Drawing.Size(100, 20);
+            this.txtEmailUer.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Photo User";
+            // 
+            // btnSelectIImage
+            // 
+            this.btnSelectIImage.Location = new System.Drawing.Point(431, 41);
+            this.btnSelectIImage.Name = "btnSelectIImage";
+            this.btnSelectIImage.Size = new System.Drawing.Size(100, 23);
+            this.btnSelectIImage.TabIndex = 12;
+            this.btnSelectIImage.Text = "Select Picture";
+            this.btnSelectIImage.UseVisualStyleBackColor = true;
+            this.btnSelectIImage.Click += new System.EventHandler(this.btnSelectIImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(574, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 115);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(548, 225);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSelectIImage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtEmailUer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtIdUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdUser;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmailUer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSelectIImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ TaskCV/ConnectLinQ/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ConnectLinQ
+{
+    public partial class Form1 : Form
+    {
+
+        /*  #region Variables
+          int ImageId = 0;
+          String strFilePath = "";
+          Image DefaultImage;
+          Byte[] ImageByteArray;
+          Database.user user = new Database.user();*/
+
+        List<Database.ConnectDatabaseDataContext> Users;
+        private object adapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // db = new DataClasses1DataContext();
+            /* var var = (from DemoLinQ in db.DemoLinQs
+                        where DemoLinQ.id == 1
+                         select  DemoLinQ).FirstOrDefault();*/
+            using (Database.ConnectDatabaseDataContext db = new Database.ConnectDatabaseDataContext())
+            {
+                dataGridView1.DataSource = db.users.Select(p => p).ToList();
+            }
+            /*MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, ImageFormat.Jpeg);
+            byte[] photo_array = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(photo_array, 0, photo_array.Length);*/
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Database.ConnectDatabaseDataContext db = new Database.ConnectDatabaseDataContext();
+            /*DemoLinQ demoLinQ = new DemoLinQ();
+            demoLinQ.id =Int32.Parse( txtId.Text);
+            demoLinQ.name = txtName.Text;
+            demoLinQ.sdt = txtPhone.Text;
+            db.DemoLinQs.InsertOnSubmit(demoLinQ);
+            db.SubmitChanges();
+            dataGridView1.DataSource = db.DemoLinQs.Select(p => p).ToList();*/
+            Database.user User = new Database.user();
+            User.idUser = Int32.Parse(txtIdUser.Text);
+            User.nameUser = txtUserName.Text;
+            User.emailUser = txtEmailUer.Text;
+
+            db.users.InsertOnSubmit(User);
+            db.SubmitChanges();
+            dataGridView1.DataSource = db.users.Select(p => p).ToList();
+
+
+        }
+        
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            String id = txtIdUser.Text;
+            Database.ConnectDatabaseDataContext db = new Database.ConnectDatabaseDataContext();
+            Database.user updateUser = db.users.Where(p => p.idUser == Convert.ToInt32(id)).SingleOrDefault();
+            updateUser.nameUser = txtUserName.Text;
+            updateUser.emailUser = txtEmailUer.Text;
+            db.SubmitChanges();
+            dataGridView1.DataSource = db.users.Select(p => p).ToList();
+        }
+
+        private void btnSelectIImage_Click(object sender, EventArgs e)
+        {
+
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box  
+                pictureBox1.Image = new Bitmap(open.FileName);
+                // image file path  
+                
+            }
+        }
+        
+        private byte[] ImageToStream(string fileName)
+        {
+            MemoryStream stream = new MemoryStream();
+        tryagain:
+            try
+            {
+                Bitmap image = new Bitmap(fileName);
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                goto tryagain;
+            }
+
+            return stream.ToArray();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+
+            txtIdUser.Text = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            txtUserName.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            txtEmailUer.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            String id = txtIdUser.Text;
+            Database.ConnectDatabaseDataContext db = new Database.ConnectDatabaseDataContext();
+            Database.user deleteUser = db.users.Where(p => p.idUser == Convert.ToInt32(id)).SingleOrDefault();
+            db.users.DeleteOnSubmit(deleteUser);
+            db.SubmitChanges();
+            dataGridView1.DataSource = db.users.Select(p => p).ToList();
+            txtIdUser.Text = "";
+            txtUserName.Text = "";
+            txtEmailUer.Text = "";
+        }
+    }
+}
